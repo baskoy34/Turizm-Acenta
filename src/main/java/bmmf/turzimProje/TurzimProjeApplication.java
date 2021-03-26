0457206957cb029ca3605d4e5caf63c012c45415
VolkanCan <volkan.baskoy@alpakyazilim.com>
2021-03-26 23:23:11 +0300

    first

---
+++ src/main/java/bmmf/turzimProje/TurzimProjeApplication.java
@@ -0,0 +1,13 @@
+package bmmf.turzimProje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurzimProjeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurzimProjeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bmmf/turzimProje/TurzimProjeApplicationTests.java
@@ -0,0 +1,13 @@
+package bmmf.turzimProje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurzimProjeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
